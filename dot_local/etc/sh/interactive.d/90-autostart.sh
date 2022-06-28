1989eafb91a469bf5822e9c32519f3ed0c1113bc
Eray Aydın <erayaydinn@protonmail.com>
2022-06-28 11:00:12 +0300

    :truck: Move zsh apps to cross shell autostart

--- dot_local/etc/sh/interactive.d/90-autostart.sh
+++ dot_local/etc/sh/interactive.d/90-autostart.sh
@@ -1,2 +1,9 @@
+# Start Neofetch
 neofetch
+
+# Initialize NVM
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Initialize SDKMAN
+[[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]] && source "$SDKMAN_DIR/bin/sdkman-init.sh"
 
